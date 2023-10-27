86b578a0b2b3d67ee47416141495ee630f0f1022
Emanuel Njiru <emanuelmutuiri@gmail.com>
2023-10-27 10:42:42 +0000

    updated my portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,27 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Student Verification System 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A web application that allows exam invigilators verify students during exams. Verification is via an exam code and student image. The application is built using the SERN stack.",
+    url: "#",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Task Manger App 🎉",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A web application that allows users to create, read, update and delete tasks. Managers can assign tasks and is in a position to monitor the progrss of various tasks. The application is built using the SERN stack.",
+    url: "#",
   },
   {
-    title: "My Resume Site",
+    title: "E-learning App 🎉",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      " A web application that allows students to subscribe, find their learning paths, get learning materials and take quizzes. Tutors on this platform upload content, monitor and examine the students who have registered for various courses. The application is built using the Django framework.",
+    url: "#",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Profile1 App 🎉",
+    description: "Created my own based portfolio using simple HTML, CSS and JavaScript on wix.com.",
+    url: "https://personalmygallery.wixsite.com/njiru-emanuel-portfolio",
   },
 ];
 
